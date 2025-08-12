5f943a07985030536ad02476c92ce7f9702d4ff0
LohitMahendrakar <168286938+LohitMahendrakar@users.noreply.github.com>
2025-08-12 18:49:20 +0530

    Time: 0 ms (100%), Space: 9.3 MB (57.85%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int f(int m, int n, vector<vector<int>> &dp){
+
+        if(m==0 && n==0) return 1;
+        if(m<0 || n<0) return 0;
+
+        if(dp[m][n] != -1) return dp[m][n];
+
+        int rigth = f(m , n-1, dp);
+        int down = f(m-1, n, dp); 
+
+        return dp[m][n] = rigth + down;
+    }
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> dp(m, vector<int>(n, -1));
+        return f(m - 1, n - 1, dp);
+    }
+};
