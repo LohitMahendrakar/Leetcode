bc21aa5032eb825a16d4860012f2db252b1a2af4
LohitMahendrakar <168286938+LohitMahendrakar@users.noreply.github.com>
2025-08-15 08:09:58 +0530

    Time: 1 ms (17.86%), Space: 11.7 MB (73.7%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,33 +1,37 @@
 class Solution {
 public:
-    int f(int m, int n, vector<vector<int>> &dp, vector<vector<int>>& ob) {
-        // Out of bounds
-        if (m < 0 || n < 0) return 0;
-
-        // Obstacle cell
-        if (ob[m][n] == 1) return 0;
-
-        // Start cell
-        if (m == 0 && n == 0) return 1;
-
-        // Memoization
-        if (dp[m][n] != -1) return dp[m][n];
-
-        int right = f(m, n - 1, dp, ob);
-        int down  = f(m - 1, n, dp, ob);
-
-        return dp[m][n] = right + down;
-    }
-
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
         int m = obstacleGrid.size();
         int n = obstacleGrid[0].size();
-
-        // If start or end cell is an obstacle
-        if (obstacleGrid[0][0] == 1 || obstacleGrid[m - 1][n - 1] == 1)
+        if (obstacleGrid[0][0] == 1 || obstacleGrid[m-1][n-1] == 1) {
             return 0;
+        }
 
-        vector<vector<int>> dp(m, vector<int>(n, -1));
-        return f(m - 1, n - 1, dp, obstacleGrid);
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+
+        // first cell
+        dp[0][0] = 1;
+
+        // first column
+        for(int i = 1; i < m; i++){
+            if(obstacleGrid[i][0] == 1) dp[i][0] = 0;
+            else dp[i][0] = dp[i-1][0];
+        }
+
+        // first row
+        for(int j = 1; j < n; j++){
+            if(obstacleGrid[0][j] == 1) dp[0][j] = 0;
+            else dp[0][j] = dp[0][j-1];
+        }
+
+        // rest of the grid
+        for(int i=1; i<m; i++){
+            for(int j=1; j<n; j++){
+                if(obstacleGrid[i][j] == 1) dp[i][j]=0;
+                else dp[i][j] = dp[i-1][j] + dp[i][j-1];
+            }
+        }
+
+        return dp[m-1][n-1];
     }
 };
